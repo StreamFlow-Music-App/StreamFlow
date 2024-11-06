07e660c1243c1a58391d2cefcf261f6d88b23dfc
FarhanSiam042 <fsh33330@gmail.com>
2024-11-06 12:12:04 +0600

    Shuffle and Sort

--- src/main/java/com/odyssey/services/ShuffleService.java
+++ src/main/java/com/odyssey/services/ShuffleService.java
@@ -5,11 +5,7 @@ import java.util.List;
 
 public class ShuffleService {
 
-    /**
-     * Shuffles the given list of songs.
-     *
-     * @param songs List of song paths to be shuffled.
-     */
+
     public void shuffleSongs(List<String> songs) {
         Collections.shuffle(songs);
     }
--- src/main/java/com/odyssey/services/SortService.java
+++ src/main/java/com/odyssey/services/SortService.java
@@ -8,21 +8,12 @@ import java.util.List;
 
 public class SortService {
 
-    /**
-     * Sorts the songs alphabetically by their names.
-     *
-     * @param songs List of song paths to be sorted.
-     */
+
     public void sortSongsByName(List<String> songs) {
         Collections.sort(songs, Comparator.comparing(this::getFileNameWithoutExtension));
     }
 
-    /**
-     * Utility method to get the file name without its extension.
-     *
-     * @param filePath Path of the file.
-     * @return File name without extension.
-     */
+
     private String getFileNameWithoutExtension(String filePath) {
         Path path = Paths.get(filePath);
         String fileName = path.getFileName().toString();
