751abebfae3b21770b5a28af0dc784b44683871b
Farhan Shahriar Haque <fsh33330@gmail.com>
2025-03-17 02:07:41 +0600

    Created HistoryService

--- src/main/java/com/odyssey/services/HistoryService.java
+++ src/main/java/com/odyssey/services/HistoryService.java
@@ -1,5 +1,26 @@
 package com.odyssey.services;
 
-public class HistoryService {
-}
+import com.odyssey.components.Song;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryService {
+    private final List<Song> history;
+
+    public HistoryService() {
+        this.history = new ArrayList<>();
+    }
+
+    public void addToHistory(Song song) {
+        history.add(song);
+    }
+
+    public List<Song> getHistory() {
+        return new ArrayList<>(history); // Return a copy to prevent external modification
+    }
+
+    public void clearHistory() {
+        history.clear();
+    }
+}
