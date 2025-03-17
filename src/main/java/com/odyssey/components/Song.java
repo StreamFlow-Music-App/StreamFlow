66788c90b1ff3ea2436f1e2b66995c51d2efb34b
Farhan Shahriar Haque <fsh33330@gmail.com>
2025-03-18 02:35:39 +0600

    Created Song.java

--- src/main/java/com/odyssey/components/Song.java
+++ src/main/java/com/odyssey/components/Song.java
@@ -1,4 +1,36 @@
 package com.odyssey.components;
 
 public class Song {
+    private String title;
+    private String artist;
+    private String genre;
+    private String album;
+
+    public Song(String title, String artist, String genre, String album) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.album = album;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    @Override
+    public String toString() {
+        return title + " - " + artist + " (" + album + ")";
+    }
 }
